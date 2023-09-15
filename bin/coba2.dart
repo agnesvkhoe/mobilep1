464e55ca810b12e966e2415b0ca53b3b453136ce
agnesvkhoe <agnesvkhoe@gmail.com>
2023-09-15 10:25:17 +0700

    first commit

---
+++ bin/coba2.dart
@@ -0,0 +1,5 @@
+import 'package:coba2/coba2.dart' as coba2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${coba2.calculate()}!');
+}
---
+++ test/coba2_test.dart
@@ -0,0 +1,8 @@
+import 'package:coba2/coba2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
